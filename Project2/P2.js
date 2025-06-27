01db3cb106d8a537d7fbd3ff1a77934cd47268f7
MuhammadKhushamAli <muhammadkhushamali79@gmail.com>
2025-06-27 12:42:13 +0500

    JS Added

--- Project2/P2.js
+++ Project2/P2.js
@@ -1,31 +1,38 @@
 const form = document.querySelector('form');
 
-form.addEventListener('submit', (event)=>{
-event.preventDefault();
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-let height = parseFloat(form.querySelector('#height').value);
-const weight = parseFloat(form.querySelector('#weight').value);
+    let height = parseFloat(form.querySelector('#height').value);
+    const weight = parseFloat(form.querySelector('#weight').value);
 
-height = height * 0.3048 // Ft to Meter
-const bmi = weight / (height * height);
-let status;
+    const result = document.createElement('h3');
+    result.setAttribute('style', 'color: white');
 
-if(bmi < 18.6)
-{
-    status = 'Under Weight';
-}
-else if(bmi >= 18.6 && bmi <= 24.9)
-{
-    status = 'Normal Rane';
-}
-else
-{
-    status = 'OverWeight';
-}
+    // Error Check
+    if (height === '' || height <= 0 || isNaN(height)) {
+        result.appendChild(document.createTextNode(`Invalid Input in Height`));
+    }
+    else if (weight === '' || weight <= 0 || isNaN(weight)) {
+        result.appendChild(document.createTextNode(`Invalid Input in Weight`));
+    }
+    else {
+        height = height * 0.3048 // Ft to Meter
+        const bmi = weight / (height * height).toFixed(2);
 
-const result = document.createElement('h3');
-result.setAttribute('style', 'color: white; font-weight: bolder;');
-result.appendChild(document.createTextNode(`Your BMI is ${bmi}
+        let status;
+        if (bmi < 18.6) {
+            status = 'Under Weight';
+        }
+        else if (bmi >= 18.6 && bmi <= 24.9) {
+            status = 'Normal Rane';
+        }
+        else {
+            status = 'OverWeight';
+        }
+
+        result.appendChild(document.createTextNode(`Your BMI is ${bmi}
     You are ${status}`));
-form.replaceWith(result);
+    }
+    form.replaceWith(result);
 })
