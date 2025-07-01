a25561775439d8fa799977019b443569cd27a475
MuhammadKhushamAli <muhammadkhushamali79@gmail.com>
2025-07-01 10:15:56 +0500

    Enhanced

--- Assignment1/api.js
+++ Assignment1/api.js
@@ -8,18 +8,21 @@ xhr.send();
 xhr.onreadystatechange = function () {
     if (xhr.readyState == 4) {
         data = JSON.parse(this.responseText);
-
-        const button = document.getElementById('fetchBtn');
-        const image = document.getElementsByTagName('img');
-        const name = document.querySelector('.card-title');
-        const text = document.querySelector('.card-text');
-        
-        button.addEventListener('click', (event) => {
-            button.remove();
-            image[0].setAttribute('src', `${data.avatar_url}`);
-            name.innerText = `${data.login}`;
-            text.innerText = data.bio;
-        })
     }
 }
 
+
+
+
+const button = document.getElementById('fetchBtn');
+const image = document.getElementsByTagName('img');
+const name = document.querySelector('.card-title');
+const text = document.querySelector('.card-text');
+button.addEventListener('click', (event) => {
+    if (data) {
+        button.remove();
+        image[0].setAttribute('src', `${data.avatar_url}`);
+        name.innerText = `${data.login}`;
+        text.innerText = data.bio;
+    }
+})
