d1184ef51400e5ec7f8b94393e10025cbbb837dc
MuhammadKhushamAli <muhammadkhushamali79@gmail.com>
2025-07-01 10:05:47 +0500

    Removing Console Log

--- Assignment1/api.js
+++ Assignment1/api.js
@@ -8,12 +8,12 @@ xhr.send();
 xhr.onreadystatechange = function () {
     if (xhr.readyState == 4) {
         data = JSON.parse(this.responseText);
-        console.log(data);
 
         const button = document.getElementById('fetchBtn');
         const image = document.getElementsByTagName('img');
         const name = document.querySelector('.card-title');
         const text = document.querySelector('.card-text');
+        
         button.addEventListener('click', (event) => {
             button.remove();
             image[0].setAttribute('src', `${data.avatar_url}`);
