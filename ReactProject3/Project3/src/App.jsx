07394959c1b0848f75e97b515c38e250001d5542
MuhammadKhushamAli <muhammadkhushamali79@gmail.com>
2025-07-18 11:26:43 +0500

    Styling

--- ReactProject3/Project3/src/App.jsx
+++ ReactProject3/Project3/src/App.jsx
@@ -22,18 +22,17 @@ function App() {
   }, [from, to]);
 
   return (
-    <div className='w-screen h-screen bg-[url(https://static.vecteezy.com/system/resources/previews/048/457/783/non_2x/currency-converter-currency-exchange-rate-dollars-to-euro-currency-trading-flat-icon-illustration-vector.jpg)]
-    bg-cover bg-no-repeat'>
-      <div>
-        <form>
+    <div className='w-screen h-screen bg-gray-500
+    bg-cover bg-no-repeat flex flex-col justify-evenly content-center flex-wrap'>
+      <h1 className='text-4xl font-serif font-black text-center uppercase'>Currency Converter</h1>
+      <div className="sm:w-[50%] max-sm:w-2xl h-[40%] bg-black opacity-50 text-white rounded-4xl flex flex-col justify-between">
           <InputField label="For" amount={amountToBeConverted} isAmountDisabled={false} isCurrencyDisabled={false}
             currencyOptions={options} onAmountChange={setAmountToBeConverted} onCurrencyChange={setFrom}
-            currency={from} />
-            <button type="button" onClick={Swap}>Swap</button>
+            currency={from} className='rounded-t-4xl'/>
+            <button type="button" onClick={Swap} className='bg-amber-300 text-black font-serif font-black hover:bg-amber-200'>Swap</button>
           <InputField label="To" amount={amountConverted} isAmountDisabled={true} isCurrencyDisabled={false}
-            currencyOptions={options} onAmountChange={setAmountConverted} onCurrencyChange={setTo}
-            currency={to} />
-        </form>
+           currencyOptions={options} onAmountChange={setAmountConverted} onCurrencyChange={setTo}
+            currency={to} className='rounded-b-4xl'/>
       </div>
     </div>
   )
--- ReactProject3/Project3/src/components/InputField.jsx
+++ ReactProject3/Project3/src/components/InputField.jsx
@@ -8,17 +8,19 @@ export default function InputField({
     currencyOptions = [],
     onAmountChange,
     onCurrencyChange,
-    currency = 'usd'
+    currency = 'usd',
+    className = ''
 }) {
     const id = useId('');
 
     return (
-        <div>
+        <div className={`flex flex-row w-full h-[40%] justify-between sm:p-[2%] max-sm:p-[5%] border-1 border-white ${className}`}>
             <div>
                 <label htmlFor={id}>
-                    <h4>{label}</h4>
+                    <h4 className="font-serif font-bold">{label}</h4>
                 </label>
                 <input
+                className="bg-white opacity-70 text-black p-[4%]"
                     type="number"
                     name="amount"
                     id={id}
@@ -33,9 +35,9 @@ export default function InputField({
 
             <div>
                 <label htmlFor="currency">
-                    <h4>Select Currency</h4>
+                    <h4 className="font-serif font-bold">Select Currency</h4>
                 </label>
-                <select className="text-white bg-black"
+                <select className="text-black bg-white opacity-60"
                     name="Currency"
                     id="currency"
                     value={currency}
