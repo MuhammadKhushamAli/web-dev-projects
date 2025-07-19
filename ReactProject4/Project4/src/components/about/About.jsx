318a41362c543d0f6b06e4eaaa90bcb0e2f7f1e6
MuhammadKhushamAli <muhammadkhushamali79@gmail.com>
2025-07-19 14:54:12 +0500

    About Added

---
+++ ReactProject4/Project4/src/components/about/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <main>
+            <article>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias cupiditate qui suscipit sapiente ex non, dolorem neque tempora atque velit dolore maiores quaerat amet consequatur? Excepturi, laboriosam at facilis impedit officiis laborum velit nisi reiciendis, natus hic sequi! Quia amet, voluptates corporis dolore totam, nobis, perferendis molestiae saepe quasi culpa repellendus iusto illum possimus pariatur! Cum, saepe sed! Voluptatum provident pariatur odit sequi soluta, quidem molestiae at aperiam! Eaque voluptatem quibusdam, maxime nobis nihil id optio excepturi possimus corrupti dolor enim suscipit. Ut provident earum, numquam dolor corporis, dolore unde harum blanditiis doloribus ex voluptatem temporibus animi quae. Odit, aperiam.</p>
+            </article>
+        </main>
+    );
+}
