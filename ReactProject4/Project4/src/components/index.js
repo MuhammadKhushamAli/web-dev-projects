5d9c320d5528afee2bee32b0c97ac994fb1d444d
MuhammadKhushamAli <muhammadkhushamali79@gmail.com>
2025-07-19 15:19:22 +0500

    Index Maintained

---
+++ ReactProject4/Project4/src/components/index.js
@@ -0,0 +1,13 @@
+import Header from "./header/Header";
+import Home from "./home/Home";
+import About from "./about/About";
+import ContactUs from "./contactUs/ContactUs";
+import GitHub from "./github/Github";
+import Footer from "./footer/Footer";
+
+export {Header};
+export {Home};
+export {About};
+export {ContactUs};
+export {GitHub};
+export {Footer};
