a235d11a2f0803881842dde79d591119c0211112
MuhammadKhushamAli <muhammadkhushamali79@gmail.com>
2025-07-19 16:23:16 +0500

    Router Created

--- ReactProject4/Project4/src/main.jsx
+++ ReactProject4/Project4/src/main.jsx
@@ -1,10 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Layout from './Layout.jsx'
+import { About, ContactUs, GitHub, Home } from './components/index.js'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children={
+      [
+        {
+          path: "",
+          element: <Home/>
+        },
+        {
+          path: "about",
+          element: <About/>
+        },
+        {
+          path: "/contact-us",
+          element: <ContactUs/>
+        },
+        {
+          path: "GitHub",
+          element:<GitHub/>
+        }
+      ]
+    }
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
