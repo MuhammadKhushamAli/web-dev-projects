4b2fe7d6a17a45ae72f919a4c5d5709043ca5f7d
MuhammadKhushamAli <muhammadkhushamali79@gmail.com>
2025-07-19 14:44:13 +0500

    Header Added

---
+++ ReactProject4/Project4/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <header>
+            <nav class="bg-red-400 h-[12vh] w-screen">
+                <ul class="w-full h-full flex flex-row justify-evenly content-center flex-wrap">
+                    <li>Hello</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                    <li>GitHub</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
