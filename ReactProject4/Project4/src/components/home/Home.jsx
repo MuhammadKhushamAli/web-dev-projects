1360b4c791df67ca18cbac91630ed5a6547a6f1a
MuhammadKhushamAli <muhammadkhushamali79@gmail.com>
2025-07-19 14:51:37 +0500

    Home Added

---
+++ ReactProject4/Project4/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <main>
+            <article className="flex flex-row">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi quibusdam, sapiente hic placeat doloremque, maxime minus voluptate tempora facilis iusto a ullam numquam ex veritatis! Quae totam saepe quo, molestias explicabo necessitatibus accusamus iste reiciendis rem, est similique enim? Cupiditate totam blanditiis enim ratione quae laboriosam nobis cumque quis aliquid harum. Inventore quos nesciunt veniam molestiae itaque fugiat, nihil corrupti rem ullam delectus, quasi atque? Dignissimos esse aliquam in, vero eligendi incidunt sint sequi veritatis labore dolorum rem, sed quasi iusto dolorem minus magnam nobis commodi blanditiis asperiores recusandae. Magni recusandae repudiandae voluptatibus facere! Dolores ducimus accusamus odit vel?</p>
+                <img src="" alt="Image 1"/>
+            </article>
+        </main>
+    )
+}
