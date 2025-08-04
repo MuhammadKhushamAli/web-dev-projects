d66351a766bdee9964f2228308519ba193726201
MuhammadKhushamAli <muhammadkhushamali79@gmail.com>
2025-08-04 14:58:05 +0500

    Storage Services Added

---
+++ ReactFinalProject/FinalProject/src/appWrite/storage.js
@@ -0,0 +1,61 @@
+import { Client, Storage, ID } from "appwrite";
+import config from "../config/config";
+
+export class StorageService{
+    #client = new Client();
+    #storage;
+
+    StorageService()
+    {
+        this.#client
+        .setEndpoint(config.api_endPoint)
+        .setProject(config.projectID);
+
+        this.#storage = new Storage(this.#client);
+    }
+
+    // It creates the file
+    async UploadFile(file)
+    {
+        try {
+            return await this.#storage.createFile(
+                config.bucketID,
+                ID.unique(),
+                file
+            );
+        } catch (error) {
+            console.error(`Error in Upload File: ${error}`);
+        }
+        return null;
+    }
+
+    // It deletes the file
+    async DeleteFile(fileId)
+    {
+        try {
+            return await this.#storage.deleteFile(
+                config.bucketID,
+                fileId
+            );
+        } catch (error) {
+            console.error(`Error in Delete File: ${error}`);
+        }
+        return null;
+    }
+
+    // It gets preview
+    async GetFilePreview(fileId)
+    {
+        try {
+            return await this.#storage.getFilePreview(
+                config.bucketID,
+                fileId
+            );
+        } catch (error) {
+            console.error(`Error in GetFilePreview: ${error}`);
+        }
+    }
+}
+
+const storage = new StorageService();
+export default storage;
