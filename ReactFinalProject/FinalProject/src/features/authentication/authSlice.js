24709e4f46c6735fe876410cdcaf10284f0b1720
MuhammadKhushamAli <muhammadkhushamali79@gmail.com>
2025-08-05 16:56:46 +0500

    Store Configed and Slice Added

---
+++ ReactFinalProject/FinalProject/src/features/authentication/authSlice.js
@@ -0,0 +1,25 @@
+import { combineSlices } from '@reduxjs/toolkit';
+
+const initialState = {
+    status: false,
+    userData: null
+};
+
+const authSlice = combineSlices({
+    name: 'authentication',
+    initialState,
+    reducer: {
+        login: (state, action) => {
+            state.status = true;
+            state.userData = action.payload;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
+    }
+});
+
+export default authSlice.reducer;
+
+export const {login, logout} = authSlice.actions;
